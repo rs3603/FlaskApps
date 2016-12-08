c813ea0df3cbd5e217fff11772024b33b94493fe
raghavendra_sirigeri <raghavendra018@gmail.com>
2016-12-07 21:08:38 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo apt-get upgrade -y
 sudo apt-get install python-dev -y
 sudo apt-get install python-pip -y
 
-sudo pip install -U opencv-python -y
+sudo pip install -U opencv-python 
 
 sudo apt-get install python-numpy python-scipy python-dev python-pip python-nose g++ libopenblas-dev git -y
 sudo pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
